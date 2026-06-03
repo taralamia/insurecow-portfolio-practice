02db3eecdab99e674014e107d151f0b186989a5d
Sha_Mohammad_Fahim <shamohammadfahim017@gmail.com>
2026-06-03 11:54:37 +0600

    Recovered stashed changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/services";
+import Projects from "@/components/projects";
+
+
+
 export default function Home() {
    return (
     <>
     <Navbar/>
      <Services/>
-     
+<Projects/>
       </>
    );     
   }
